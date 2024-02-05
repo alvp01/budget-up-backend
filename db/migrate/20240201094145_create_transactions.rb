6f9ed45616c75504d55b94b2b8d886c5c11376a8
Abel Lavieri <abel.lavieri@gmail.com>
2024-02-05 19:15:11 -0400

    Add budget_id to transaction model

--- db/migrate/20240201094145_create_transactions.rb
+++ db/migrate/20240201094145_create_transactions.rb
@@ -3,12 +3,14 @@ class CreateTransactions < ActiveRecord::Migration[7.1]
     create_table :transactions do |t|
       t.datetime :date
       t.string   :note
-      t.float    :amount,  null: false, default: 0.0
-      t.integer  :item_id, null: false
+      t.float    :amount,    null: false, default: 0.0
+      t.integer  :item_id,   null: false
+      t.integer  :budget_id, null: false
 
       t.timestamps
     end
 
     add_index :transactions, :item_id
+    add_index :transactions, :budget_id
   end
 end
