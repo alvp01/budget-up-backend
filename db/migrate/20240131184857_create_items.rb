bd1c09dc9d342c257ce4d0de8c18dc52dd2e08f4
Abel Lavieri <abel.lavieri@gmail.com>
2024-02-05 19:14:33 -0400

    Change variable name to remaining_amount for item model

--- db/migrate/20240131184857_create_items.rb
+++ db/migrate/20240131184857_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
       t.float   :planned_amount,    null: false, default: 0.0
-      t.float   :allocated_amount,  null: false, default: 0.0
+      t.float   :remaining_amount,  null: false, default: 0.0
       t.integer :item_type,         null: false, default: 0
       t.string  :label
       t.integer :category_id
