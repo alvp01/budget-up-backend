fdbb643139fc253ee844f67bc1ad4f7118b8da66
Abel Lavieri <abel.lavieri@gmail.com>
2024-03-12 23:32:55 -0400

    Change category migration

--- db/migrate/20240131175230_create_categories.rb
+++ db/migrate/20240131175230_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
-      t.string  :category_type, null: false
+      t.string  :category_type
       t.belongs_to :budget
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_130515) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "category_type", null: false
+    t.string "category_type"
     t.bigint "budget_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
